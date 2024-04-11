f04d3a84cffae3d8c8eb172876980e1db8c7a38d
legendsayantan <legendsayantanpaul@gmail.com>
2024-04-11 12:34:24 +0530

    fixed case of title

--- src/header/Homelink.js
+++ src/header/Homelink.js
@@ -4,7 +4,7 @@ import React from "react";
 function Homelink({homeclick}) {
     //get the base url
     let url = window.location.hostname;
-    let name = url.replace("legendsayantan", "LegendSayantan");
+    let name = url.replace("legend", "Legend").replace("sayantan", "Sayantan");
     return (
         <div id="bottom_layer_homelink" onClick={homeclick}>
             <div id={"top_layer_homelink"}>
